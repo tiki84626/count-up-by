62e05812f471604add57aecbbfa80e9cc7437e97
Andrew Malkin and Jeremy Fryd <andrew@>
2016-08-16 09:03:14 -0700

    implement javascript logic for count up by

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,15 @@ $(document).ready(function() {
   $("form").submit(function(event) {
     var countTo = parseInt($("input#count-to").val());
     var countBy = parseInt($("input#count-by").val());
+    var output = [];
 
     if (countBy <= countTo && countBy > 0) {
-
-      alert(countBy);
-
-    } else {
-
+      for (var index = countBy; index <= countTo; index += countBy) {
+        output.push(index);
+      }
     }
+
+    alert(output);
 
     event.preventDefault();
   });
